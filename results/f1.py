6b083732762295c27526487ad882f6edc959cfd7
Surya Nattuva <surya.sai0809@gmail.com>
2022-05-05 23:25:17 -0400

    Added model history plot

--- results/f1.py
+++ results/f1.py
@@ -68,3 +68,423 @@ weighted avg       0.72      0.73      0.72        99
 weighted avg       0.72      0.75      0.73        99
 
 
+2022-05-05 22:05:46.632558
+              precision    recall  f1-score   support
+
+        Idle       0.85      0.90      0.88        51
+       Cross       0.00      0.00      0.00         4
+        Hook       0.71      0.71      0.71        17
+         Jab       0.62      0.50      0.56        10
+    Uppercut       0.74      0.82      0.78        17
+
+    accuracy                           0.78        99
+   macro avg       0.58      0.59      0.58        99
+weighted avg       0.75      0.78      0.76        99
+
+
+2022-05-05 22:11:46.677849
+              precision    recall  f1-score   support
+
+        Idle       0.87      0.90      0.88        51
+       Cross       0.00      0.00      0.00         4
+        Hook       0.91      0.59      0.71        17
+         Jab       0.50      0.50      0.50        10
+    Uppercut       0.62      0.88      0.73        17
+
+    accuracy                           0.77        99
+   macro avg       0.58      0.57      0.57        99
+weighted avg       0.76      0.77      0.75        99
+
+
+2022-05-05 22:14:55.478055
+              precision    recall  f1-score   support
+
+        Idle       0.87      0.90      0.88        51
+       Cross       0.00      0.00      0.00         4
+        Hook       0.65      0.65      0.65        17
+         Jab       0.44      0.40      0.42        10
+    Uppercut       0.80      0.94      0.86        17
+
+    accuracy                           0.78        99
+   macro avg       0.55      0.58      0.56        99
+weighted avg       0.74      0.78      0.76        99
+
+
+2022-05-05 22:16:09.727334
+              precision    recall  f1-score   support
+
+        Idle       0.90      0.84      0.87        31
+       Cross       0.50      0.50      0.50         2
+        Hook       0.45      0.38      0.42        13
+         Jab       0.67      0.50      0.57         8
+    Uppercut       0.44      0.67      0.53        12
+
+    accuracy                           0.67        66
+   macro avg       0.59      0.58      0.58        66
+weighted avg       0.69      0.67      0.67        66
+
+
+2022-05-05 22:16:47.006982
+              precision    recall  f1-score   support
+
+        Idle       0.80      0.75      0.77        16
+       Cross       0.00      0.00      0.00         2
+        Hook       0.40      0.29      0.33         7
+         Jab       0.00      0.00      0.00         2
+    Uppercut       0.50      0.83      0.62         6
+
+    accuracy                           0.58        33
+   macro avg       0.34      0.37      0.35        33
+weighted avg       0.56      0.58      0.56        33
+
+
+2022-05-05 22:17:58.183121
+              precision    recall  f1-score   support
+
+        Idle       0.90      0.88      0.89        51
+       Cross       0.00      0.00      0.00         4
+        Hook       0.79      0.65      0.71        17
+         Jab       0.56      0.50      0.53        10
+    Uppercut       0.64      0.94      0.76        17
+
+    accuracy                           0.78        99
+   macro avg       0.58      0.59      0.58        99
+weighted avg       0.76      0.78      0.76        99
+
+
+2022-05-05 22:20:11.472683
+              precision    recall  f1-score   support
+
+        Idle       0.90      0.84      0.87        51
+       Cross       0.00      0.00      0.00         4
+        Hook       0.38      0.29      0.33        17
+         Jab       0.42      0.50      0.45        10
+    Uppercut       0.48      0.71      0.57        17
+
+    accuracy                           0.66        99
+   macro avg       0.44      0.47      0.45        99
+weighted avg       0.65      0.66      0.65        99
+
+
+2022-05-05 22:24:03.133594
+              precision    recall  f1-score   support
+
+        Idle       0.84      0.96      0.90        51
+       Cross       0.00      0.00      0.00         4
+        Hook       0.69      0.53      0.60        17
+         Jab       0.67      0.60      0.63        10
+    Uppercut       0.74      0.82      0.78        17
+
+    accuracy                           0.79        99
+   macro avg       0.59      0.58      0.58        99
+weighted avg       0.75      0.79      0.76        99
+
+
+2022-05-05 22:25:22.883883
+              precision    recall  f1-score   support
+
+        Idle       0.87      0.80      0.84        51
+       Cross       0.00      0.00      0.00         4
+        Hook       0.47      0.47      0.47        17
+         Jab       0.41      0.70      0.52        10
+    Uppercut       0.56      0.59      0.57        17
+
+    accuracy                           0.67        99
+   macro avg       0.46      0.51      0.48        99
+weighted avg       0.67      0.67      0.66        99
+
+
+2022-05-05 22:26:21.668767
+              precision    recall  f1-score   support
+
+        Idle       0.86      0.96      0.91        51
+       Cross       0.50      0.25      0.33         4
+        Hook       0.86      0.71      0.77        17
+         Jab       0.67      0.60      0.63        10
+    Uppercut       0.88      0.88      0.88        17
+
+    accuracy                           0.84        99
+   macro avg       0.75      0.68      0.71        99
+weighted avg       0.83      0.84      0.83        99
+
+
+2022-05-05 22:26:57.789316
+              precision    recall  f1-score   support
+
+        Idle       0.85      0.92      0.89        51
+       Cross       0.00      0.00      0.00         4
+        Hook       0.30      0.18      0.22        17
+         Jab       0.26      0.50      0.34        10
+    Uppercut       0.53      0.47      0.50        17
+
+    accuracy                           0.64        99
+   macro avg       0.39      0.41      0.39        99
+weighted avg       0.61      0.64      0.62        99
+
+
+2022-05-05 22:27:25.120225
+              precision    recall  f1-score   support
+
+        Idle       0.83      0.86      0.85        51
+       Cross       0.00      0.00      0.00         4
+        Hook       0.43      0.18      0.25        17
+         Jab       0.45      0.50      0.48        10
+    Uppercut       0.52      0.82      0.64        17
+
+    accuracy                           0.67        99
+   macro avg       0.45      0.47      0.44        99
+weighted avg       0.64      0.67      0.64        99
+
+
+2022-05-05 22:34:12.246808
+              precision    recall  f1-score   support
+
+        Idle       0.82      0.90      0.86        51
+       Cross       0.00      0.00      0.00         4
+        Hook       0.50      0.35      0.41        17
+         Jab       0.38      0.50      0.43        10
+    Uppercut       0.56      0.59      0.57        17
+
+    accuracy                           0.68        99
+   macro avg       0.45      0.47      0.46        99
+weighted avg       0.64      0.68      0.66        99
+
+
+2022-05-05 22:35:24.274376
+              precision    recall  f1-score   support
+
+        Idle       0.52      1.00      0.68        51
+       Cross       0.00      0.00      0.00         4
+        Hook       0.00      0.00      0.00        17
+         Jab       0.00      0.00      0.00        10
+    Uppercut       0.00      0.00      0.00        17
+
+    accuracy                           0.52        99
+   macro avg       0.10      0.20      0.14        99
+weighted avg       0.27      0.52      0.35        99
+
+
+2022-05-05 22:42:28.493541
+              precision    recall  f1-score   support
+
+        Idle       0.85      0.88      0.87        51
+       Cross       1.00      0.25      0.40         4
+        Hook       0.76      0.76      0.76        17
+         Jab       0.64      0.70      0.67        10
+    Uppercut       0.88      0.88      0.88        17
+
+    accuracy                           0.82        99
+   macro avg       0.83      0.70      0.72        99
+weighted avg       0.82      0.82      0.81        99
+
+
+2022-05-05 22:43:05.403036
+              precision    recall  f1-score   support
+
+        Idle       0.86      0.94      0.90        51
+       Cross       0.50      0.25      0.33         4
+        Hook       0.78      0.82      0.80        17
+         Jab       0.62      0.50      0.56        10
+    Uppercut       1.00      0.88      0.94        17
+
+    accuracy                           0.84        99
+   macro avg       0.75      0.68      0.70        99
+weighted avg       0.83      0.84      0.83        99
+
+
+2022-05-05 22:43:42.570152
+              precision    recall  f1-score   support
+
+        Idle       0.84      0.90      0.87        51
+       Cross       0.00      0.00      0.00         4
+        Hook       0.75      0.71      0.73        17
+         Jab       0.25      0.20      0.22        10
+    Uppercut       0.83      0.88      0.86        17
+
+    accuracy                           0.76        99
+   macro avg       0.53      0.54      0.53        99
+weighted avg       0.73      0.76      0.74        99
+
+
+2022-05-05 22:44:23.089248
+              precision    recall  f1-score   support
+
+        Idle       0.52      1.00      0.68        51
+       Cross       0.00      0.00      0.00         4
+        Hook       0.00      0.00      0.00        17
+         Jab       0.00      0.00      0.00        10
+    Uppercut       0.00      0.00      0.00        17
+
+    accuracy                           0.52        99
+   macro avg       0.10      0.20      0.14        99
+weighted avg       0.27      0.52      0.35        99
+
+
+2022-05-05 22:46:04.549992
+              precision    recall  f1-score   support
+
+        Idle       0.52      1.00      0.68        51
+       Cross       0.00      0.00      0.00         4
+        Hook       0.00      0.00      0.00        17
+         Jab       0.00      0.00      0.00        10
+    Uppercut       0.00      0.00      0.00        17
+
+    accuracy                           0.52        99
+   macro avg       0.10      0.20      0.14        99
+weighted avg       0.27      0.52      0.35        99
+
+
+2022-05-05 22:46:36.514513
+              precision    recall  f1-score   support
+
+        Idle       0.52      1.00      0.68        51
+       Cross       0.00      0.00      0.00         4
+        Hook       0.00      0.00      0.00        17
+         Jab       0.00      0.00      0.00        10
+    Uppercut       0.00      0.00      0.00        17
+
+    accuracy                           0.52        99
+   macro avg       0.10      0.20      0.14        99
+weighted avg       0.27      0.52      0.35        99
+
+
+2022-05-05 22:48:02.334781
+              precision    recall  f1-score   support
+
+        Idle       0.52      1.00      0.68        51
+       Cross       0.00      0.00      0.00         4
+        Hook       0.00      0.00      0.00        17
+         Jab       0.00      0.00      0.00        10
+    Uppercut       0.00      0.00      0.00        17
+
+    accuracy                           0.52        99
+   macro avg       0.10      0.20      0.14        99
+weighted avg       0.27      0.52      0.35        99
+
+
+2022-05-05 22:50:23.782394
+              precision    recall  f1-score   support
+
+        Idle       0.52      1.00      0.68        51
+       Cross       0.00      0.00      0.00         4
+        Hook       0.00      0.00      0.00        17
+         Jab       0.00      0.00      0.00        10
+    Uppercut       0.00      0.00      0.00        17
+
+    accuracy                           0.52        99
+   macro avg       0.10      0.20      0.14        99
+weighted avg       0.27      0.52      0.35        99
+
+
+2022-05-05 22:59:51.497928
+              precision    recall  f1-score   support
+
+        Idle       0.52      1.00      0.68        51
+       Cross       0.00      0.00      0.00         4
+        Hook       0.00      0.00      0.00        17
+         Jab       0.00      0.00      0.00        10
+    Uppercut       0.00      0.00      0.00        17
+
+    accuracy                           0.52        99
+   macro avg       0.10      0.20      0.14        99
+weighted avg       0.27      0.52      0.35        99
+
+
+2022-05-05 23:00:12.936395
+              precision    recall  f1-score   support
+
+        Idle       0.81      0.76      0.79        51
+       Cross       0.00      0.00      0.00         4
+        Hook       0.57      0.47      0.52        17
+         Jab       0.32      0.80      0.46        10
+    Uppercut       0.75      0.53      0.62        17
+
+    accuracy                           0.65        99
+   macro avg       0.49      0.51      0.48        99
+weighted avg       0.68      0.65      0.65        99
+
+
+2022-05-05 23:00:51.080934
+              precision    recall  f1-score   support
+
+        Idle       0.83      0.96      0.89        51
+       Cross       1.00      0.25      0.40         4
+        Hook       0.92      0.65      0.76        17
+         Jab       0.70      0.70      0.70        10
+    Uppercut       0.88      0.88      0.88        17
+
+    accuracy                           0.84        99
+   macro avg       0.87      0.69      0.73        99
+weighted avg       0.85      0.84      0.83        99
+
+
+2022-05-05 23:01:40.313922
+              precision    recall  f1-score   support
+
+        Idle       0.82      0.92      0.87        51
+       Cross       0.50      0.25      0.33         4
+        Hook       0.69      0.65      0.67        17
+         Jab       0.33      0.20      0.25        10
+    Uppercut       0.83      0.88      0.86        17
+
+    accuracy                           0.77        99
+   macro avg       0.64      0.58      0.60        99
+weighted avg       0.74      0.77      0.75        99
+
+
+2022-05-05 23:02:15.130577
+              precision    recall  f1-score   support
+
+        Idle       0.83      0.94      0.88        51
+       Cross       0.00      0.00      0.00         4
+        Hook       0.92      0.71      0.80        17
+         Jab       0.50      0.50      0.50        10
+    Uppercut       0.83      0.88      0.86        17
+
+    accuracy                           0.81        99
+   macro avg       0.62      0.61      0.61        99
+weighted avg       0.78      0.81      0.79        99
+
+
+2022-05-05 23:05:31.818835
+              precision    recall  f1-score   support
+
+        Idle       0.85      0.92      0.89        51
+       Cross       0.00      0.00      0.00         4
+        Hook       0.73      0.65      0.69        17
+         Jab       0.60      0.60      0.60        10
+    Uppercut       0.84      0.94      0.89        17
+
+    accuracy                           0.81        99
+   macro avg       0.61      0.62      0.61        99
+weighted avg       0.77      0.81      0.79        99
+
+
+2022-05-05 23:08:18.957435
+              precision    recall  f1-score   support
+
+        Idle       0.87      0.80      0.84        51
+       Cross       0.67      0.50      0.57         4
+        Hook       0.69      0.53      0.60        17
+         Jab       0.44      0.70      0.54        10
+    Uppercut       0.60      0.71      0.65        17
+
+    accuracy                           0.72        99
+   macro avg       0.65      0.65      0.64        99
+weighted avg       0.74      0.72      0.72        99
+
+
+2022-05-05 23:09:23.225446
+              precision    recall  f1-score   support
+
+        Idle       0.78      0.82      0.80        39
+       Cross       0.00      0.00      0.00         4
+        Hook       0.36      0.33      0.34        15
+         Jab       0.20      0.33      0.25         9
+    Uppercut       0.62      0.50      0.55        16
+
+    accuracy                           0.58        83
+   macro avg       0.39      0.40      0.39        83
+weighted avg       0.57      0.58      0.57        83
+
+
